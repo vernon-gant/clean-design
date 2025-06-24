abd272c3e4693d52141295e9e89cb6e768e4398f
Aleksandr Zakharov <aleks.zakharov@pm.me>
2025-06-24 22:26:40 +0200

    new(task02): BankAkkound without negative balance check

---
+++ 2. WorkingCodeStillHasBugs/app/src/main/java/com/example/task02/BankAccount.java
@@ -0,0 +1,21 @@
+package com.example.task02;
+
+public class BankAccount {
+    private double balance;
+    
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
